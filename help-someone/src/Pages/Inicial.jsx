9272098836ed6c13ed08cfdbe7effd9508a987aa
iroDevs <ironegrolol@gamil.com>
2021-10-09 12:28:43 -0300

    atualizando pag inicial

--- help-someone/src/Pages/Inicial.jsx
+++ help-someone/src/Pages/Inicial.jsx
@@ -1,5 +1,6 @@
 import React, {useContext,useEffect,useState} from 'react'
 import Button from 'react-bootstrap/Button';
+import Footer from '../Components/Footer'
 import './css/inicial.css'
 
 
@@ -7,21 +8,24 @@ import './css/inicial.css'
 export default function Inicial() {
   const [profile , setProfile] = useState('')
   return (
-    <div className="principal-page">
+    <div className="principal-page ">
+     
       <div className="left-side">
-
+        <h1 className="left-side-text">Precisa de ajuda?</h1>
         <div className="left-button">
           <Button variant="outline-primary" size="lg">Preciso de ajuda</Button>
       </div>
       </div>
         <div className="right-side">
-
+          <h1 className="right-side-text">Quer Ajudar?</h1>
         <div className="right-button">
 
-        <Button variant="outline-dark" size="lg">Eu Posso Ajudar</Button>
+        <Button variant="outline-light" size="lg">Eu Posso Ajudar</Button>
 
             </div>
           </div>
+          <Footer />
         </div>
+        
   )
 }
