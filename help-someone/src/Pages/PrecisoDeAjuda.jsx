3bfa7dfa87a88c5325ae6448819c47d30893b779
iroDevs <ironegrolol@gamil.com>
2021-10-10 12:43:46 -0300

    logica do nome ou nickname

--- help-someone/src/Pages/PrecisoDeAjuda.jsx
+++ help-someone/src/Pages/PrecisoDeAjuda.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react'
 import Footer from '../Components/Footer'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import "./css/PrecisoDeAjuda.css"
 
 export default function PrecisoDeAjuda() {
+const [nome , setNome] = useState(false)
+
+function NameOrNickName(){
+  setNome(!nome);
+  console.log("essssee",nome);
+}
+
+
+
+
+
+const type = 'checkbox'
   return (
    <div>
      <div className="Help-form">
       <Form>
         <div className="itens-form">
-        <label>
-          Nome:
-           <Form.Control size="lg" type="text" placeholder="Nome" required />
-           
+        <Form.Check 
+        type={type}
+        id={`default-${type}`}
+        label={`Anonimato?`}
+        onClick={NameOrNickName}
+      />
+      {nome ? 
+         <label>
+         NickName:
+           <Form.Control size="lg" type="text" placeholder="Apelido" required />
+        </label> 
+        :
+         <label>
+           Nome:
+           <Form.Control size="lg" type="text" placeholder="Seu Nome" required />
         </label>
+    
+    
+    }
+      <br></br>
+         
+       
       </div>
           
         <label>
-          Sobrenome:
+         Meio de pagamento
            <Form.Control size="lg" type="text" placeholder="Sobrenome" required />
         </label>
 
