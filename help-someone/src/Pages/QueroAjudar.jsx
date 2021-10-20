f900f298acd53da670548d6c49908a44055c0458
iroDevs <ironegrolol@gamil.com>
2021-10-20 17:05:50 -0300

    add

---
+++ help-someone/src/Pages/QueroAjudar.jsx
@@ -0,0 +1,10 @@
+import Footer from "../Components/Footer";
+import React,{useContext} from "react";
+import DataContext from "../Context/DataContext";
+
+export default function QueroAjudar() {
+  // const { ajuda } = useContext(DataContext)
+  return (
+    <Footer></Footer>
+  )
+}
